c7071e313d4c3862994b6c11362efe45711726d5
AKI-86 <201310899+AKI-86@users.noreply.github.com>
2025-04-16 13:46:15 +0000

    Ad.商品一覧、登録、詳細途中まで完了

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,20 +1,38 @@
 class Admin::ItemsController < ApplicationController
-  before_action :authenticate_admin!
+  # before_action :authenticate_admin!
   def index
+    @items = Item.all
   end
 
   def new
+    @item = Item.new
+    @genres = Genre.all
   end
 
   def create
+    @item = Item.new(item_params)
+    @genres = Genre.all
+    if @item.save
+      redirect_to admin_items_path, notice: '商品が登録されました。'
+    else
+      render :new
+    end
   end
 
   def show
+    @item = Item.find(params[:id])
   end
 
   def edit
+    @item = Item.find(params[:id])
   end
 
   def update
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :introduction, :genre_id, :price, :image, :is_active)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,12 @@
 class Item < ApplicationRecord
   has_one_attached :image
+  belongs_to :genre
+
+  validates :name, :introduction, :genre_id, :price, presence: true
+  validates :is_active, inclusion: { in: [true, false] }
+  validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+  def active_status_label
+    is_active ? "販売中" : "販売停止中"
+  end
 end
