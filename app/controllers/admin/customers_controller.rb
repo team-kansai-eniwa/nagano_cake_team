821d6b0f1e57211fbf7d67acb6aa4634af4292a1
AKI-86 <201310899+AKI-86@users.noreply.github.com>
2025-04-18 08:00:36 +0000

    任意のコミット名

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,7 +1,8 @@
 class Admin::CustomersController < ApplicationController
-  before_action :authenticate_admin!
+  # before_action :authenticate_admin!
   
   def index
+    @orders = Order.page(params[:page])
   end
 
   def show
--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -21,6 +21,7 @@ class Admin::ItemsController < ApplicationController
 
   def show
     @item = Item.find(params[:id])
+    
   end
 
   def edit
