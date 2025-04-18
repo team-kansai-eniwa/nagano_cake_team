c9d7db892a364a854ab40953a69699be89ebc61a
AKI-86 <201310899+AKI-86@users.noreply.github.com>
2025-04-18 03:47:44 +0000

    任意のコミット名

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,9 +1,14 @@
 class Public::ItemsController < ApplicationController
 
   def index
+    @items = Item.page(params[:page]).per(8)
+    @genres = Genre.all
+    @items_all = Item.all
   end
 
   def show
+    @item = Item.find(params[:id])
+    @genres = Genre.all
   end
 
   end
--- app/models/item.rb
+++ app/models/item.rb
@@ -5,6 +5,7 @@ class Item < ApplicationRecord
   validates :name, :introduction, :genre_id, :price, presence: true
   validates :is_active, inclusion: { in: [true, false] }
   validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :image, presence: true
 
   def active_status_label
     is_active ? "販売中" : "販売停止中"
