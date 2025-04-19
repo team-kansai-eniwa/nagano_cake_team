d968438d845ec8518dc0114496d2be84a2f100be
EC2 Default User <ec2-user@ip-172-31-40-77.ap-northeast-1.compute.internal>
2025-04-19 12:18:21 +0000

    顧客情報登録

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -2,12 +2,15 @@ class Admin::CustomersController < ApplicationController
   before_action :authenticate_admin!
   
   def index
+    @customers = Customer.page(params[:page])
   end
 
   def show
+    @customer = Customer.find(params[:id])
   end
 
   def edit
+    @customer = Customer.find(params[:id])
   end
 
   def update
