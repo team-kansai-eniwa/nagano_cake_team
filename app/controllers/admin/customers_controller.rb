47b074d4654853b10e05b20db7cbb59e45a15fd8
AKI-86 <201310899+AKI-86@users.noreply.github.com>
2025-04-19 00:59:49 +0000

    カートに入れる機能の実装、注文履歴ページ一部追加ページ

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,11 +1,15 @@
 class Admin::CustomersController < ApplicationController
   # before_action :authenticate_admin!
   
+  #order_detailsのデータが保存されていないため現在は表示されない
   def index
     @orders = Order.page(params[:page])
+    @order_details = OrderDetail.page(params[:page])
   end
 
   def show
+    @orders = Order.page(params[:page])
+    @order_details = OrderDetail.page(params[:page])
   end
 
   def edit
