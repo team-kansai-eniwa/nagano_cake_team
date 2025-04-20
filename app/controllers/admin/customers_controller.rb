a337c87d773ca45b12c2b52c6a4a6b8d4c01470b
EC2 Default User <ec2-user@ip-172-31-40-77.ap-northeast-1.compute.internal>
2025-04-20 05:48:48 +0000

    修正済（あとレイアウトのみ）

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,7 +1,6 @@
 class Admin::CustomersController < ApplicationController
-  # before_action :authenticate_admin!
+  before_action :authenticate_admin!
   
-  #order_detailsのデータが保存されていないため現在は表示されない
   def index
     @customers = Customer.page(params[:page])
   end
@@ -15,5 +14,17 @@ class Admin::CustomersController < ApplicationController
   end
 
   def update
+    @customer = Customer.find(params[:id])
+    if @customer.update(customer_params)
+      redirect_to admin_customer_path(@customer)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :email, :postal_code, :address, :telephone_number, :is_active)
   end
 end
