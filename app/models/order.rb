dddfcb3fe3822ae38950583a3284c6abfba1de85
AKI-86 <201310899+AKI-86@users.noreply.github.com>
2025-04-21 04:11:13 +0000

    会員側レイアウト一部修正、管理者側注文履歴一部実装

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,7 @@
 class Order < ApplicationRecord
   belongs_to :customer
   has_many :order_details
+  has_many :item, through: :order_details
 
   enum payment_method: { credit_card: 0, transfer: 1 }
   enum status: { waiting: 0, paid_up: 1, producting: 2, preparing: 3, sented: 4 }
