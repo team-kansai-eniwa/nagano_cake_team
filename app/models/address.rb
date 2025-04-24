28f92baa333c819837ca9d372523839995804df6
AKI-86 <201310899+AKI-86@users.noreply.github.com>
2025-04-24 03:36:00 +0000

    Ad.レイアウト編集

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,7 +1,7 @@
 class Address < ApplicationRecord
   belongs_to :customer
 
-
+  validates :name, :postal_code, :address, presence: true
   def address_display
     '〒' + self.postal_code + ' ' + self.address + ' ' + self.name
   end
